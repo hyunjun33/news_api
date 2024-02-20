30c58fe4d5e6350cc3b5e5757d1c89c238f837bf
Hyunjun Kim <hyunjun.kim133@gmail.com>
2024-02-20 20:21:47 +0900

    fifth commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const API_KEY = `84b8c0ef44af454f9c4a4135a5cc3ab9`;
 let news = [];
 
 const getNews = async () => {
-    let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+    let url = new URL(`https://cheery-centaur-e0cea7.netlify.app/`);
     let response = await fetch(url);
     let data = await response.json();
     news = data.articles;
