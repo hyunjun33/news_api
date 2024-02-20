1bd6b981d54b1b2d6dd00f9cd31023af2b83b7b9
Hyunjun Kim <hyunjun.kim133@gmail.com>
2024-02-20 20:43:06 +0900

    fifth commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const API_KEY = `84b8c0ef44af454f9c4a4135a5cc3ab9`;
 let news = [];
 
 const getNews = async () => {
-    let url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=아이유`);
+    let url = new URL(`https://cheery-centaur-e0cea7.netlify.app/top-headlines?q=아이유&country=kr&apiKey=${API_KEY}`);
     let response = await fetch(url);
     console.log(response);
     let data = await response.json();
