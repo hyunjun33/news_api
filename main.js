809b20cee4c857a2c00c414536446911bb83d747
Hyunjun Kim <hyunjun.kim133@gmail.com>
2024-02-20 20:14:11 +0900

    third commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-const API_KEY = `84b8c0ef44af454f9c4a4135a5cc3ab9`;
+const API_KEY = `#############################`;
 let news = [];
 
 const getNews = async () => {
-    let url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=kr&apikey=${API_KEY}`);
+    let url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
     let response = await fetch(url);
     let data = await response.json();
     news = data.articles;
