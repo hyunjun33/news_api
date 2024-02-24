01f3adc7de6b4cd7522cae7c48dc11017ea5c388
Hyunjun Kim <hyunjun.kim133@gmail.com>
2024-02-24 18:50:59 +0900

    add pagination function

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 
+
+// URL 및 API_KEY 정보
 const API_KEY = `84b8c0ef44af454f9c4a4135a5cc3ab9`;
-// let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`); // News API
-let url = new URL(`https://cheery-centaur-e0cea7.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`); // 누나 API
+let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`); // News API
+// let url = new URL(`https://cheery-centaur-e0cea7.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`); // 누나 API
 
 
 let newsList = [];
@@ -10,13 +12,23 @@ const menus = document.querySelectorAll(".menus button");
 const sideMenus = document.querySelectorAll(".side-menu-list button")
 
 
+
+// 카테고리 별 생성한 버튼에 클릭 이벤트 추가
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
 sideMenus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
 
 
-const getNews = async () => {    
+// 페이지네이션 구현을 위한 변수 선언
+let totalResults = 0;
+let currentPage = 1;
+const pageSize = 10; // 한 페이지에 보여줄 콘텐츠 개수
+const groupSize = 5; // 몇 페이지씩 보여줄 것인지
 
+
+const getNews = async () => {    
     try {
+        url.searchParams.set("page", currentPage); // URL에 현재 페이지 값과 페이지 사이즈 값을 추가
+        url.searchParams.set("pageSize", pageSize);
 
         const response = await fetch(url);
         const data = await response.json();
@@ -26,7 +38,11 @@ const getNews = async () => {
         }
         else if (response.status === 200) {
             newsList = data.articles;
+            totalResults = data.totalResults
+            console.log("totalResults: ", totalResults);
+            
             render();
+            paginationRender();
         }
         else {
             throw new Error(data.message)
@@ -42,27 +58,24 @@ const getNews = async () => {
 
 
 const getNewsByCategory = async (event) => {
-
     const category = event.target.textContent.toLowerCase();
-    // url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`); // News API
-    url = new URL(`https://cheery-centaur-e0cea7.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`); // 누나 API (과제 제출용)
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`); // News API
+    // url = new URL(`https://cheery-centaur-e0cea7.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`); // 누나 API (과제 제출용)
 
     getNews();
 };
 
 
 const getNewsByKeyword = async () => {
-
     const keyword = searchInput.value.toLowerCase();
-    // url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`); // News API
-    url = new URL(`https://cheery-centaur-e0cea7.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`); // 누나 API (과제 제출용)
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`); // News API
+    // url = new URL(`https://cheery-centaur-e0cea7.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`); // 누나 API (과제 제출용)
 
     getNews();
 };
 
 
 const errorRender = (errorMessage) => {
-
     const errorHtml = `
     <div class="alert alert-warning" role="alert">
         ${errorMessage}
@@ -74,7 +87,6 @@ const errorRender = (errorMessage) => {
 
 
 const render = () => {
-
     const newsHtml = newsList.map((news) => 
     `
     <div class="row news">
@@ -105,18 +117,78 @@ const render = () => {
 };
 
 
-const openNav = () => {
+const paginationRender = () => {
+    // 알아야 하는 정보 totalResults, currentPage, pageSize, groupSize, totalPageNumber, pageGroupNumber, lastPageNumber, firstPageNumber
+    const totalPageNumber = Math.ceil(totalResults / pageSize);
+    const pageGroupNumber = Math.ceil(currentPage / groupSize);
+    
+    let lastPageNumber = pageGroupNumber * groupSize; // 마지막 페이지 숫자가 전체 페이지 개수보다 크다면, 마지막 페이지를 totalPageNumber로 설정해야 함
+    if (totalPageNumber < lastPageNumber) {
+        lastPageNumber = totalPageNumber
+    };
 
+    let firstPageNumber = lastPageNumber - (groupSize - 1);
+    if (firstPageNumber <= 0) {
+        firstPageNumber = 1
+    };
+
+
+    let nextPage = currentPage + 1;
+    if (nextPage >= totalPageNumber) {
+        nextPage = totalPageNumber
+    };
+    
+    let prevPage = currentPage - 1;
+    if (prevPage <= 0) {
+        prevPage = firstPageNumber
+    };
+
+    let paginationHtml = ``;
+    
+    paginationHtml += 
+    `
+        <li class="page-item">
+            <a class="page-link" onclick="moveToPage(${prevPage})"><</a>
+        </li>    
+    `
+
+
+    for (let i = firstPageNumber; i <= lastPageNumber; i++) {
+        paginationHtml += 
+        `
+            <li class="page-item ${i===currentPage?'active':''}">
+                <a class="page-link" onclick="moveToPage(${i})">${i}</a>
+            </li>
+        `
+    };
+
+    paginationHtml += 
+    `
+        <li class="page-item">
+            <a class="page-link" onclick="moveToPage(${nextPage})">></a>
+        </li>    
+    `   
+
+
+    document.querySelector(".pagination").innerHTML = paginationHtml
+};
+
+
+const moveToPage = (pageNumber) => {
+    console.log("pageNumber:", pageNumber);
+    currentPage = pageNumber
+    getNews();
+}
+
+const openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";
 };
   
 const closeNav = () => {
-
     document.getElementById("mySidenav").style.width = "0";
 };
 
 const openSearchBox = () => {
-    
     let inputArea = document.getElementById("input-area");
     console.log(inputArea.style.display);
 
